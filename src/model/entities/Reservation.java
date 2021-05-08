2aff517fd16ae176a59155d63be99320f88a4c65
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-05-08 14:38:36 -0300

    Using java exception

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -41,18 +41,17 @@ public class Reservation {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) {
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return " Reservation dates for updates must be future dates";
+			throw new IllegalArgumentException(" Reservation dates for updates must be future dates");
 		} 
 		if (!checkOut.after(checkIn)) {
-			return "Check-out date must be after check-in date.";
+			throw new IllegalArgumentException("Check-out date must be after check-in date.");
 
 		} 	
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
 	}
 	
 	@Override
