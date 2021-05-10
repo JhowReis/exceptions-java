06508cb639f72a6e53e4a39944a50c51bc8da297
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-05-10 17:37:56 -0300

    Good solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
@@ -41,8 +42,11 @@ public class Program {
 		catch(ParseException e) {
 			System.out.println("Invalid date format");
 		}
-		catch(IllegalArgumentException e) {
+		catch(DomainException e) {
 			System.out.println("Error in reservation: " + e.getMessage());
+		}
+		catch(RuntimeException e) {
+			System.out.println("Unexpected error");
 		}
 
 		sc.close();
